a308b6df204f23f2e0ec00bd651156c3d0ba2193
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-06-17 11:28:10 +0530

    len

---
+++ length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int main(){
+//     string txt = "ananthakrishnan";
+//     cout << txt.length();
+//     return 0;
+// }
+
+int main(){
+    string txt = "anandhu";
+    cout << txt.size();
+}
